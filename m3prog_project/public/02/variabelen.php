110ac22b162fb6f52e83beb0041e79cca8de1ec1
chrissie0079 <144009863+chrissie0079@users.noreply.github.com>
2024-03-05 14:39:50 +0100

    les2.2

---
+++ m3prog_project/public/02/variabelen.php
@@ -0,0 +1,24 @@
+<?php
+$auto2 = 23;
+$_naam = "christian";
+$auto = 3;
+$auto/onderdelen = "motor"
+$huis&tuin = "schuur";
+$naw = "?";
+$22	= 22;
+$$auto = "BMW";
+$kleurVanAuto = "zwart";
+$software_dev = "opleiding";
+
+echo $auto2;
+echo $_naam;
+echo $auto;
+// echo $auto/onderdelen;
+// echo $huis&tuin;
+echo $naw;
+//echo $22;
+echo $$auto;
+echo $kleurVanAuto;
+echo $software_dev;
+
+?>
