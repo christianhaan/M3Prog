74785e1f7027360500e57cda25b72b441abbf960
chrissie0079 <144009863+chrissie0079@users.noreply.github.com>
2024-03-05 13:37:14 +0100

    les2.1

---
+++ m3prog_project/public/02/datatypes.php
@@ -0,0 +1,8 @@
+<?php>
+$huisnummer = 43;
+$uitslag_rijbewijs = true;
+$benzine = "De prijs voor 1 liter Euro 95 benzine is gemiddeld €1,80"
+$niks = null;
+
+echo  " mijn huisnummer is: " . $huisnummer;
+?>
